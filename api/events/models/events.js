b6977ffd33febecd6c5dd2325b6890d1492d4557
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2022-11-22 19:42:42 +0000

    add dependencies from name to slug

---
+++ api/events/models/events.js
@@ -0,0 +1,33 @@
+/* const slugify = require("slugify");
+
+module.exports = {
+  lifecycles: {
+    beforeCreate: async (data) => {
+      if (data.name) {
+        data.slug = slugify(data.name, { lower: true });
+      }
+    },
+    beforeUpdate: async (params, data) => {
+      if (data.name) {
+        data.slug = slugify(data.name, { lower: true });
+      }
+    },
+  },
+}; */
+
+const slugify = require("slugify");
+
+module.exports = {
+  beforeCreate(event) {
+    const { data } = event.params;
+    if (data.name) {
+      data.slug = slugify(data.name, { lower: true });
+    }
+  },
+  beforeUpdate(event) {
+    const { data } = event.params;
+    if (data.name) {
+      data.slug = slugify(data.name, { lower: true });
+    }
+  },
+};
