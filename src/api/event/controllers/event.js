6757893f345553d6d2d3a3a59e7b71263aacec9f
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2022-11-25 11:34:07 +0000

    Update gitignore

--- src/api/event/controllers/event.js
+++ src/api/event/controllers/event.js
@@ -1,9 +1,86 @@
-'use strict';
-
 /**
- * event controller
+ *  event controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+ const {createCoreController} = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::event.event');
+ module.exports = createCoreController('api::event.event');
+ 
+/*  module.exports = createCoreController('api::event.event', ({strapi}) => ({
+   //Find with populate ----------------------------------------
+   async find(ctx) {
+     const populateList = [
+       'image',
+       'user',
+     ]
+     // Push any additional query params to the array
+     populateList.push(ctx.query.populate)
+     ctx.query.populate = populateList.join(',')
+     // console.log(ctx.query)
+     const content = await super.find(ctx)
+     return content
+   },
+ 
+   // Create user event----------------------------------------
+   async create(ctx) {
+     let entity;
+     ctx.request.body.data.user = ctx.state.user;
+     entity = await super.create(ctx);
+     return entity;
+   },
+   // Update user event----------------------------------------
+   async update(ctx) {
+     let entity;
+     const {id} = ctx.params;
+     const query = {
+       filters: {
+         id: id,
+         user: {id: ctx.state.user.id}
+       }
+     };
+     const events = await this.find({query: query});
+     console.log(events);
+     if (!events.data || !events.data.length) {
+       return ctx.unauthorized(`You can't update this entry`);
+     }
+     entity = await super.update(ctx)
+     return entity;
+   },
+ 
+   // Delete a user event----------------------------------------
+   async delete(ctx) {
+     const {id} = ctx.params;
+     const query = {
+       filters: {
+         id: id,
+         user: {id: ctx.state.user.id}
+       }
+     }
+     const events = await this.find({query: query});
+     if (!events.data || !events.data.length) {
+       return ctx.unauthorized(`You can't delete this entry`);
+     }
+     const response = await super.delete(ctx);
+     return response;
+   },
+   // Get logged in users----------------------------------------
+   async me(ctx) {
+     const user = ctx.state.user;
+     if (!user) {
+       return ctx.badRequest(null, [
+         {messages: [{id: "No authorization header was found"}]},
+       ]);
+     }
+     const query = {
+       filters: {
+         user: {id: user.id}
+       }
+     }
+     const data = await this.find({query: query});
+     if (!data) {
+       return ctx.notFound();
+     }
+     const sanitizedEntity = await this.sanitizeOutput(data, ctx);
+     return this.transformResponse(sanitizedEntity);
+   },
+ })); */
--- src/api/event/routes/event.js
+++ src/api/event/routes/event.js
@@ -4,6 +4,71 @@
  * event router
  */
 
+ /* module.exports = {
+    routes: [
+        {
+          method: "GET",
+          path: "/events",
+          handler: "events.find",
+          config: {
+            policies: []
+          }
+        },
+        {
+          method: "GET",
+          path: "/events/me",
+          handler: "events.me",
+          config: {
+            policies: []
+          }
+        },
+        {
+          method: "GET",
+          path: "/events/count",
+          handler: "events.count",
+          config: {
+            policies: []
+          }
+        },
+        {
+          method: "GET",
+          path: "/events/:id",
+          handler: "events.findOne",
+          config: {
+            policies: []
+          }
+        },
+        {
+          method: "POST",
+          path: "/events",
+          handler: "events.create",
+          config: {
+            policies: []
+          }
+        },
+        {
+          method: "PUT",
+          path: "/events/:id",
+          handler: "events.update",
+          config: {
+            policies: []
+          }
+        },
+        {
+          method: "DELETE",
+          path: "/events/:id",
+          handler: "events.delete",
+          config: {
+            policies: []
+          }
+        }
+      ]
+  } */
+
+
+
+
+
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::event.event');
